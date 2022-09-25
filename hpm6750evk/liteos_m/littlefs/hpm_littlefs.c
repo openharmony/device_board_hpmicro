6294d3dfc7f4bcc470e274db2be5b310217fbc9e
Hongpeng Huo <hongpeng.huo@hpmicro.com>
2022-09-25 23:52:37 +0800

    adapter liteos newest littlefs framework
    Signed-off-by: Hongpeng Huo <hongpeng.huo@hpmicro.com>

--- hpm6750evk/liteos_m/littlefs/hpm_littlefs.c
+++ hpm6750evk/liteos_m/littlefs/hpm_littlefs.c
@@ -22,25 +22,27 @@
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <los_fs.h>
 
-struct HpmLittlefsCfg {
-    struct lfs_config cfg;
-    struct HpmLittleCtx ctx;
-};
-
-static struct HpmLittlefsCfg g_hpmLittlefsCfgs[] = {
+struct HpmLittlefsCfg g_hpmLittlefsCfgs[] = {
     [0] = {
         .cfg = {
-            .read  = HpmLittlefsRead,
-            .prog  = HpmLittlefsProg,
-            .erase = HpmLittlefsErase,
-            .sync  = HpmLittlefsSync,
+            /* partition low-level read func */
+            .readFunc = HpmLittlefsRead,
+            /* partition low-level write func */
+            .writeFunc = HpmLittlefsProg,
+            /* partition low-level erase func */
+            .eraseFunc = HpmLittlefsErase,
 
-            .read_size = 16,
-            .prog_size = 16,
-            .cache_size = 1024,
-            .lookahead_size = 16,
-            .block_cycles = 1000,
+            .readSize = 16,
+            .writeSize = 16,
+            .blockSize = 0, /* auto fill in HpmLittlefsDriverInit() */
+            .blockCount = 0, /* auto fill in HpmLittlefsDriverInit() */
+            .cacheSize = 1024,
+
+            .partNo = 0,
+            .lookaheadSize = 16,
+            .blockCycles = 1000,
         },
         .ctx = {
             .startOffset = 5 * 1024 * 1024,
@@ -55,10 +57,20 @@ void HpmLittlefsInit(void)
 {
     uint32_t num = sizeof(g_hpmLittlefsCfgs) / sizeof(g_hpmLittlefsCfgs[0]);
     int ret;
-    for (int i = 0; i < num; i++) {
-        g_hpmLittlefsCfgs[i].cfg.context = &g_hpmLittlefsCfgs[i].ctx;
-        HpmLittlefsDriverInit(&g_hpmLittlefsCfgs[i].cfg);
 
+    int *lengthArray = (int *)malloc(num * sizeof(int) * 2);
+    int *addrArray = lengthArray + num;
+
+    for (int i = 0; i < num; i++) {
+        lengthArray[i] = g_hpmLittlefsCfgs[i].ctx.len;
+        addrArray[i] = g_hpmLittlefsCfgs[i].ctx.startOffset;
+    }
+
+    LOS_DiskPartition("spiflash", "littlefs", lengthArray, addrArray, num);
+    free(lengthArray);
+
+    for (int i = 0; i < num; i++) {
+        HpmLittlefsDriverInit(&g_hpmLittlefsCfgs[i]);
         ret = mount(NULL, g_hpmLittlefsCfgs[i].ctx.mountPoint, "littlefs", 0, &g_hpmLittlefsCfgs[i].cfg);
         if (ret < 0) {
             printf("Err: hpm littlefs [%s] mount failed!!!\n", g_hpmLittlefsCfgs[i].ctx.mountPoint);
