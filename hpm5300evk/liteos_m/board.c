81d3bd73011ba9c8c31ccb1aa0291761d56b44de
商镪 <shangqiang1750@163.com>
2024-01-15 13:42:05 +0800

    board:add support for board hpm5300evk
    Change-Id: Iedd335eec48073e6240693b2fa15bb38c361342f
    Signed-off-by: 商镪 <shangqiang1750@163.com>

---
+++ hpm5300evk/liteos_m/board.c
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2022 HPMicro
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "board.h"
+#include <hpm_pllctl_drv.h>
+#include "hpm_pllctlv2_drv.h"
+#include <hpm_gpio_drv.h>
+#include "hpm_pcfg_drv.h"
+#include <hpm_pllctlv2_drv.h>
+/**
+ * @brief FLASH configuration option definitions:
+ * option[0]:
+ *    [31:16] 0xfcf9 - FLASH configuration option tag
+ *    [15:4]  0 - Reserved
+ *    [3:0]   option words (exclude option[0])
+ * option[1]:
+ *    [31:28] Flash probe type
+ *      0 - SFDP SDR / 1 - SFDP DDR
+ *      2 - 1-4-4 Read (0xEB, 24-bit address) / 3 - 1-2-2 Read(0xBB, 24-bit address)
+ *      4 - HyperFLASH 1.8V / 5 - HyperFLASH 3V
+ *      6 - OctaBus DDR (SPI -> OPI DDR)
+ *      8 - Xccela DDR (SPI -> OPI DDR)
+ *      10 - EcoXiP DDR (SPI -> OPI DDR)
+ *    [27:24] Command Pads after Power-on Reset
+ *      0 - SPI / 1 - DPI / 2 - QPI / 3 - OPI
+ *    [23:20] Command Pads after Configuring FLASH
+ *      0 - SPI / 1 - DPI / 2 - QPI / 3 - OPI
+ *    [19:16] Quad Enable Sequence (for the device support SFDP 1.0 only)
+ *      0 - Not needed
+ *      1 - QE bit is at bit 6 in Status Register 1
+ *      2 - QE bit is at bit1 in Status Register 2
+ *      3 - QE bit is at bit7 in Status Register 2
+ *      4 - QE bit is at bit1 in Status Register 2 and should be programmed by 0x31
+ *    [15:8] Dummy cycles
+ *      0 - Auto-probed / detected / default value
+ *      Others - User specified value, for DDR read, the dummy cycles should be 2 * cycles on FLASH datasheet
+ *    [7:4] Misc.
+ *      0 - Not used
+ *      1 - SPI mode
+ *      2 - Internal loopback
+ *      3 - External DQS
+ *    [3:0] Frequency option
+ *      1 - 30MHz / 2 - 50MHz / 3 - 66MHz / 4 - 80MHz / 5 - 100MHz / 6 - 120MHz / 7 - 133MHz / 8 - 166MHz
+ *
+ * option[2] (Effective only if the bit[3:0] in option[0] > 1)
+ *    [31:20]  Reserved
+ *    [19:16] IO voltage
+ *      0 - 3V / 1 - 1.8V
+ *    [15:12] Pin group
+ *      0 - 1st group / 1 - 2nd group
+ *    [11:8] Connection selection
+ *      0 - CA_CS0 / 1 - CB_CS0 / 2 - CA_CS0 + CB_CS0 (Two FLASH connected to CA and CB respectively)
+ *    [7:0] Drive Strength
+ *      0 - Default value
+ * option[3] (Effective only if the bit[3:0] in option[0] > 2, required only for the QSPI NOR FLASH that not supports
+ *              JESD216)
+ *    [31:16] reserved
+ *    [15:12] Sector Erase Command Option, not required here
+ *    [11:8]  Sector Size Option, not required here
+ *    [7:0] Flash Size Option
+ *      0 - 4MB / 1 - 8MB / 2 - 16MB
+ */
+#if defined(FLASH_XIP) && FLASH_XIP
+__attribute__ ((section(".nor_cfg_option"))) const uint32_t option[4] = {0xfcf90002, 0x00000006, 0x00001000, 0x00000001};
+#endif
+
+void board_delay_us(uint32_t us)
+{
+    clock_cpu_delay_us(us);
+}
+
+void board_delay_ms(uint32_t ms)
+{
+    clock_cpu_delay_ms(ms);
+}
+
+void board_init_clock(void)
+{
+    uint32_t cpu0_freq = clock_get_frequency(clock_cpu0);
+
+    if (cpu0_freq == PLLCTL_SOC_PLL_REFCLK_FREQ) {
+        /* Configure the External OSC ramp-up time: ~9ms */
+        pllctlv2_xtal_set_rampup_time(HPM_PLLCTLV2, 32UL * 1000UL * 9U);
+
+        /* Select clock setting preset1 */
+        sysctl_clock_set_preset(HPM_SYSCTL, 2);
+    }
+
+    /* group0[0] */
+    clock_add_to_group(clock_cpu0, 0);
+    clock_add_to_group(clock_ahb, 0);
+    clock_add_to_group(clock_lmm0, 0);
+    clock_add_to_group(clock_mchtmr0, 0);
+    clock_add_to_group(clock_rom, 0);
+    clock_add_to_group(clock_can0, 0);
+    clock_add_to_group(clock_can1, 0);
+    clock_add_to_group(clock_can2, 0);
+    clock_add_to_group(clock_can3, 0);
+    clock_add_to_group(clock_ptpc, 0);
+    clock_add_to_group(clock_lin0, 0);
+    clock_add_to_group(clock_lin1, 0);
+    clock_add_to_group(clock_lin2, 0);
+    clock_add_to_group(clock_lin3, 0);
+    clock_add_to_group(clock_gptmr0, 0);
+    clock_add_to_group(clock_gptmr1, 0);
+    clock_add_to_group(clock_gptmr2, 0);
+    clock_add_to_group(clock_gptmr3, 0);
+    clock_add_to_group(clock_i2c0, 0);
+    clock_add_to_group(clock_i2c1, 0);
+    clock_add_to_group(clock_i2c2, 0);
+    clock_add_to_group(clock_i2c3, 0);
+    clock_add_to_group(clock_spi0, 0);
+    clock_add_to_group(clock_spi1, 0);
+    clock_add_to_group(clock_spi2, 0);
+    clock_add_to_group(clock_spi3, 0);
+    clock_add_to_group(clock_uart0, 0);
+    clock_add_to_group(clock_uart1, 0);
+    clock_add_to_group(clock_uart2, 0);
+    clock_add_to_group(clock_uart3, 0);
+    clock_add_to_group(clock_uart4, 0);
+    clock_add_to_group(clock_uart5, 0);
+    clock_add_to_group(clock_uart6, 0);
+    /* group0[1] */
+    clock_add_to_group(clock_uart7, 0);
+    clock_add_to_group(clock_watchdog0, 0);
+    clock_add_to_group(clock_watchdog1, 0);
+    clock_add_to_group(clock_mbx0, 0);
+    clock_add_to_group(clock_tsns, 0);
+    clock_add_to_group(clock_crc0, 0);
+    clock_add_to_group(clock_adc0, 0);
+    clock_add_to_group(clock_adc1, 0);
+    clock_add_to_group(clock_dac0, 0);
+    clock_add_to_group(clock_dac1, 0);
+    clock_add_to_group(clock_acmp, 0);
+    clock_add_to_group(clock_opa0, 0);
+    clock_add_to_group(clock_opa1, 0);
+    clock_add_to_group(clock_mot0, 0);
+    clock_add_to_group(clock_rng, 0);
+    clock_add_to_group(clock_sdp, 0);
+    clock_add_to_group(clock_kman, 0);
+    clock_add_to_group(clock_gpio, 0);
+    clock_add_to_group(clock_hdma, 0);
+    clock_add_to_group(clock_xpi0, 0);
+    clock_add_to_group(clock_usb0, 0);
+
+    /* Connect Group0 to CPU0 */
+    clock_connect_group_to_cpu(0, 0);
+
+    /* Bump up DCDC voltage to 1175mv */
+    pcfg_dcdc_set_voltage(HPM_PCFG, 1175);
+
+    /* Configure CPU to 480MHz, AXI/AHB to 160MHz */
+    sysctl_config_cpu0_domain_clock(HPM_SYSCTL, clock_source_pll0_clk0, 2, 3);
+    /* Configure PLL0 Post Divider */
+    pllctlv2_set_postdiv(HPM_PLLCTLV2, 0, 0, 0);    /* PLL0CLK0: 960MHz */
+    pllctlv2_set_postdiv(HPM_PLLCTLV2, 0, 1, 3);    /* PLL0CLK1: 600MHz */
+    pllctlv2_set_postdiv(HPM_PLLCTLV2, 0, 2, 7);    /* PLL0CLK2: 400MHz */
+    /* Configure PLL0 Frequency to 960MHz */
+    pllctlv2_init_pll_with_freq(HPM_PLLCTLV2, 0, 960000000);
+
+    clock_update_core_clock();
+
+    /* Configure mchtmr to 24MHz */
+    clock_set_source_divider(clock_mchtmr0, clk_src_osc24m, 1);
+}
+
+void board_init_pmp(void)
+{
+
+}
+
+void board_init(void)
+{
+    board_init_clock();
+    sysctl_set_cpu_lp_mode(HPM_SYSCTL, HPM_CORE0, cpu_lp_mode_ungate_cpu_clock);
+    board_init_pmp();
+}
+
+void board_print_clock_freq(void)
+{
+    printf("==============================\r\n");
+    printf(" %s clock summary\r\n", "HPM5300EVK");
+    printf("==============================\r\n");
+    printf("cpu0:\t\t %luHz\n", clock_get_frequency(clock_cpu0));
+    printf("ahb:\t\t %luHz\n", clock_get_frequency(clock_ahb));
+    printf("mchtmr0:\t %luHz\n", clock_get_frequency(clock_mchtmr0));
+    printf("xpi0:\t\t %luHz\n", clock_get_frequency(clock_xpi0));
+    printf("==============================\r\n");
+}
+
+void board_print_banner(void)
+{
+    const uint8_t banner[] = {"\n\
+----------------------------------------------------------------------\r\n\
+$$\\   $$\\ $$$$$$$\\  $$\\      $$\\ $$\\\r\n\
+$$ |  $$ |$$  __$$\\ $$$\\    $$$ |\\__|\r\n\
+$$ |  $$ |$$ |  $$ |$$$$\\  $$$$ |$$\\  $$$$$$$\\  $$$$$$\\   $$$$$$\\\r\n\
+$$$$$$$$ |$$$$$$$  |$$\\$$\\$$ $$ |$$ |$$  _____|$$  __$$\\ $$  __$$\\\r\n\
+$$  __$$ |$$  ____/ $$ \\$$$  $$ |$$ |$$ /      $$ |  \\__|$$ /  $$ |\r\n\
+$$ |  $$ |$$ |      $$ |\\$  /$$ |$$ |$$ |      $$ |      $$ |  $$ |\r\n\
+$$ |  $$ |$$ |      $$ | \\_/ $$ |$$ |\\$$$$$$$\\ $$ |      \\$$$$$$  |\r\n\
+\\__|  \\__|\\__|      \\__|     \\__|\\__| \\_______|\\__|       \\______/\r\n\
+----------------------------------------------------------------------\r\n"};
+    printf("%s", banner);
+}
+
---
+++ hpm5300evk/liteos_m/littlefs/hpm_littlefs.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2022 HPMicro
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <hpm_clock_drv.h>
+#include <stdio.h>
+#include <los_interrupt.h>
+#include "hpm_littlefs_drv.h"
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <los_fs.h>
+
+struct HpmLittlefsCfg g_hpmLittlefsCfgs[] = {
+    [0] = {
+        .cfg = {
+            /* partition low-level read func */
+            .readFunc = HpmLittlefsRead,
+            /* partition low-level write func */
+            .writeFunc = HpmLittlefsProg,
+            /* partition low-level erase func */
+            .eraseFunc = HpmLittlefsErase,
+
+            .readSize = 16,
+            .writeSize = 16,
+            .blockSize = 0, /* auto fill in HpmLittlefsDriverInit() */
+            .blockCount = 0, /* auto fill in HpmLittlefsDriverInit() */
+            .cacheSize = 1024,
+
+            .partNo = 0,
+            .lookaheadSize = 16,
+            .blockCycles = 1000,
+        },
+        .ctx = {
+            .startOffset = 512 * 1024,
+            .len = 16 * 1024,
+            .base = HPM_XPI0_BASE,
+            .mountPoint = "/data",
+        }
+    },
+};
+
+void HpmLittlefsInit(void)
+{
+    uint32_t num = sizeof(g_hpmLittlefsCfgs) / sizeof(g_hpmLittlefsCfgs[0]);
+    int ret;
+
+    int *lengthArray = (int *)malloc(num * sizeof(int) * 2);
+    int *addrArray = lengthArray + num;
+
+    for (int i = 0; i < num; i++) {
+        lengthArray[i] = g_hpmLittlefsCfgs[i].ctx.len;
+        addrArray[i] = g_hpmLittlefsCfgs[i].ctx.startOffset;
+    }
+
+    LOS_DiskPartition("spiflash", "littlefs", lengthArray, addrArray, num);
+    free(lengthArray);
+
+    for (int i = 0; i < num; i++) {
+        HpmLittlefsDriverInit(&g_hpmLittlefsCfgs[i]);
+        ret = mount(NULL, g_hpmLittlefsCfgs[i].ctx.mountPoint, "littlefs", 0, &g_hpmLittlefsCfgs[i].cfg);
+        if (ret < 0) {
+            printf("Err: hpm littlefs [%s] mount failed!!!\n", g_hpmLittlefsCfgs[i].ctx.mountPoint);
+            continue;
+        }
+
+        DIR *dir = NULL;
+        if ((dir = opendir(g_hpmLittlefsCfgs[i].ctx.mountPoint)) == NULL) {
+            ret = mkdir(g_hpmLittlefsCfgs[i].ctx.mountPoint, S_IRUSR | S_IWUSR);
+            if (ret) {
+                printf("Err: hpm littlefs mkdir [%s] mount failed!!!\n", g_hpmLittlefsCfgs[i].ctx.mountPoint);
+                continue;
+            }
+        } else {
+            closedir(dir);
+        }
+    }
+}
+
